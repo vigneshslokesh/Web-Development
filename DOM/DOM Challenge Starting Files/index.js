d58e18fe1dc1aa5e88b84e2a2507f346b975b8e1
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-08 14:52:38 +0530

    Dice Game

--- DOM/DOM Challenge Starting Files/index.js
+++ DOM/DOM Challenge Starting Files/index.js
@@ -1,1 +1,3 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+document.querySelector("button").style.backgroundColor="yellow";
+document.querySelector("h1").classList.add("huge");
