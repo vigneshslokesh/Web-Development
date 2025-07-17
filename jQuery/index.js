b6532eebfd1a609b858e1e5902eecdb22dc1b46d
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-17 15:59:09 +0530

    jQuery basics

---
+++ jQuery/index.js
@@ -0,0 +1,22 @@
+// $("h1").css("color","red");
+// $("h1").text("Bye")
+// $("button").html("<em>Don't click</em>")
+
+// $("h1").addClass("big-title margin-50");
+
+// $(document).keypress(function(event){
+//     $("h1").text(event.key)
+// })
+
+
+// $("h1").on("mouseover", function(){
+//     $("h1").css("color","purple");
+// });
+
+
+
+// alert("Working");
+
+// document.querySelector(".btn").addEventListener("click", function(){
+//     alert("First button pressed.");
+// })
