36e395743b79f68140b69a3b1e555d68a37cb3ea
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-17 16:02:38 +0530

    jQuery Animation

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,7 +2,7 @@
 // $("h1").text("Bye")
 // $("button").html("<em>Don't click</em>")
 
-// $("h1").addClass("big-title margin-50");
+$("h1").addClass("big-title margin-50");
 
 // $(document).keypress(function(event){
 //     $("h1").text(event.key)
@@ -13,10 +13,12 @@
 //     $("h1").css("color","purple");
 // });
 
-
-
 // alert("Working");
 
 // document.querySelector(".btn").addEventListener("click", function(){
 //     alert("First button pressed.");
 // })
+
+$("button").on("click", function(){
+    $("h1").animate({opacity:0.5, margin:10});
+})
