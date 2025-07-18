82fc0b485db0408df1af04b6687aea820f4f8762
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-18 12:30:26 +0530

    Simon Game- Start and Level

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,17 +2,34 @@ var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var started = false;
+var level = 0;
+
+
+$(document).on("keypress",function(){
+    if(!started){
+         $("#level-title").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+   
+})
+
 
 $(".btn").on("click", function(){
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    $("#"+userChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
     playSound(userChosenColour);
-    console.log(userClickedPattern);
+    // console.log(userClickedPattern);
+    animatePress(userChosenColour);
 });
 
 
 function nextSequence(){
+    level++;
+    $("#level-title").text("Level " + level);
+
     var n = Math.random();
     var randomNumber = Math.floor(n*4);
     // console.log(randomNumber);
@@ -29,5 +46,14 @@ function playSound(name){
     var audio = new Audio("sounds/"+name+".mp3");
     audio.play();
 }
+
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+
+    setTimeout(function(){
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
 
 // console.log(randomChosenColour);
