f12edabdd763321d21f4eb8a224775dd26e3c093
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-22 17:40:16 +0530

    Reading and Writing File using NodeJS

--- Backend/2.2 Native Modules/index.js
+++ Backend/2.2 Native Modules/index.js
@@ -5,7 +5,17 @@ const fs = require("fs");
 //     console.log("The file has been saved.")
 // });
 
-fs.readFile("./message.txt","utf-8", (err,data) =>{
+// fs.readFile("./message.txt","utf-8", (err,data) =>{
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+fs.writeFile("new_file.txt","www.google.com",(err)=>{
     if (err) throw err;
-    console.log(data);
+    console.log("File created.")
+});
+
+fs.readFile("./new_file.txt","utf-8",(err,data)=>{
+    if (err) throw err;
+    console.log(data)
 });
