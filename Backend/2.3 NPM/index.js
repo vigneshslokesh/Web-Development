80e6d6749eed7cf85b512016df65d99052fe3475
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-22 19:05:07 +0530

    QR - generator

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require("sillyname");
+
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is: ${sillyName}`);
+
+import {randomSuperhero} from "superheroes";
+var superhero = randomSuperhero();
+console.log(`My name is: ${superhero}`);
