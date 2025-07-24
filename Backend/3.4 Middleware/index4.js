25303473b90572b34e371bb0bc254fc82a8a86cf
vigneshslokesh <vigneshslokesh0612@gmail.com>
2025-07-24 15:52:31 +0530

    BandName Gen - ExpressJS

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,7 +1,30 @@
 import express from "express";
+import { dirname} from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+var bandname = "";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(bodyParser.urlencoded({extended: true}));
+
+function bandNameGenerator(req,res,next){
+  console.log(req.body);
+  bandname = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req,res) =>{
+  res.send(`The Bandname is ${bandname}`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
